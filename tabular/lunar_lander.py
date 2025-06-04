c672877f13bce11fb0d0f13b88a5247b7b549b49
matteoventali <ventali.1985026@studenti.uniroma1.it>
2025-06-04 09:51:31 +0200

    Media mobile

--- tabular/lunar_lander.py
+++ tabular/lunar_lander.py
@@ -14,7 +14,7 @@ def discretize(obs):
         result = []
         
         # Interval array
-        x_intervals     = [-1.0 -0.5, 0.5, 1.0]
+        x_intervals     = [-0.5, 0.5]
         y_intervals     = [-0.1, 0.1, 1.5]
         vx_intervals    = [-7.5, -5, -0.3, -0.1, 0.1, 0.3, 5, 7.5]
         vy_intervals    = [-7.5, -5, -0.3, -0.1, 0.1, 0.3, 5, 7.5]
@@ -35,7 +35,7 @@ def discretize(obs):
     
 
 class QLearner():
-    def __init__(self, env:gym.Env, max_episodes=100000, gamma=0.9, alpha=0.1, end_eps=0.01, start_eps=1.0,  eps_decay=0.999):
+    def __init__(self, env:gym.Env, max_episodes=5000, gamma=0.9, alpha=0.1, end_eps=0.01, start_eps=1.0,  eps_decay=0.999):
         self.env = env
         self.max_episodes = max_episodes        
         self.gamma = gamma
@@ -43,7 +43,7 @@ class QLearner():
         self.end_eps = end_eps
         self.eps = start_eps
         self.eps_decay = eps_decay
-        self.policy_name = 'best_policy_intID=2'
+        self.policy_name = 'policy_lunar_lander'
         
     
     def _espilon_update(self):
@@ -71,18 +71,21 @@ class QLearner():
         s = discretize(s)
 
         # Array for collecting total rewards
-        total_rewards = [0] * self.max_episodes
+        total_rewards = []
         
         # Training of episodes
         for n_episode in range(self.max_episodes):
+            
             terminated = truncated = False
+            episode_reward = 0
+            
             while not (terminated or truncated): # Single episode
                 # Select the action to be executed
                 a = self._next_action(s, modality)
 
                 # Execution of a
                 ns, reward, terminated, truncated, _ = self.env.step(a)
-                total_rewards[n_episode] += reward
+                episode_reward += reward
                 ns = discretize(ns)
 
                 # Update Q_table
@@ -95,20 +98,21 @@ class QLearner():
                     s = ns
             
             # Stats of the episode
-            print(f"(episode {n_episode} {total_rewards[n_episode]})")
+            print(f"(episode {n_episode} {episode_reward})")
             self._espilon_update()
             s, _ = self.env.reset()
             s = discretize(s)
+            total_rewards.append(episode_reward)
 
         # Dump q_table
-        with open("policies/lunar_lander/" + self.policy_name, "wb") as f:
+        with open(self.policy_name, "wb") as f:
             pickle.dump(dict(ql.q_table), f)
 
-        return total_rewards[-50:]
+        return total_rewards
             
 
     def load_policy(self):
-        with open("policies/lunar_lander/" + self.policy_name, "rb") as f:
+        with open(self.policy_name, "rb") as f:
             self.policy = defaultdict(lambda: np.zeros(self.env.action_space.n),pickle.load(f))
         
     
@@ -144,16 +148,13 @@ if __name__ == "__main__":
     
     # Training
     ql = QLearner(env)
-    
-    #rw_eps = ql.tabular_QLearning()
-    print("\nRestarting training")
     #rw_random = ql.tabular_QLearning(0)
-    
-    # Results plot
-    """ plt.plot(rw_random, label='Random policy')
-    plt.plot(rw_eps, label='Epsilon Greedy policy')
-    plt.show()"""
+    #print("\nRestarting training")
+    rw_eps = ql.tabular_QLearning()
+    #
+    ## Results plot
+    #plt.plot(np.convolve(rw_random, np.ones(50)/50), label='Random policy')
+    plt.plot(np.convolve(rw_eps, np.ones(50)/50), label='Epsilon Greedy policy')
+    plt.show()
 
-    ql.run_policy()
-
-    #print(discretize((-5.1, 0, 6.3, 7.1, 2.544, 4.1, 0, 0)))
+    #ql.run_policy()
