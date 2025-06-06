95265455f97136871aee54c0a23558bd90994460
matteoventali <ventali.1985026@studenti.uniroma1.it>
2025-06-06 10:41:18 +0200

    reorganizin src.

--- dqn/lunar_lander_tensorflow.py
+++ dqn/lunar_lander_tensorflow.py
@@ -1,14 +1,21 @@
 # Authors: Matteo Ventali and Valerio Spagnoli
-# ML Project : DQN for Lunar Lander environment
+# ML Project : DQN for Lunar Lander envinronment
 
+#from google.colab import drive
+import os
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import gymnasium as gym
 import random as ran
+from gymnasium.spaces import Box
 from collections import deque
-import torch
-import torch.nn as nn
-import torch.optim as optim
+from pprint import pprint
+from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential #type: ignore
+from tensorflow.keras.layers import Dense, Normalization #type: ignore
 
 window_size = 500
 
@@ -70,37 +77,32 @@ class ReplayBuffer:
 
 
 class DQN:
-    def __init__(self, state_dim, num_actions, device="cpu"):
-        self.device = device
-        self.model = nn.Sequential(
-            nn.Linear(state_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, num_actions)
-        ).to(self.device)
-        self.loss_fn = nn.MSELoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+    def __init__(self, state_dim, num_actions):
+        # Creation of the neural network
+        self.model = Sequential([
+            Dense(32, activation='relu', input_shape=(8,)),
+            Dense(32, activation='relu'),
+            Dense(num_actions)  # output: Q(s,a) for all a
+        ])
+        self.model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(0.001))
 
     def train(self, batch):
-        states = torch.tensor(np.array([s for s, q in batch]), dtype=torch.float32).to(self.device)
-        q_targets = torch.tensor(np.array([q for s, q in batch]), dtype=torch.float32).to(self.device)
+        # Preparing the batch separating the state and the target values
+        states = np.array([s for s, q in batch])   
+        q_targets = np.array([q for s, q in batch])
 
-        self.optimizer.zero_grad()
-        q_preds = self.model(states)
-        loss = self.loss_fn(q_preds, q_targets)
-        loss.backward()
-        self.optimizer.step()
+        # Fit
+        self.model.fit(states, q_targets, verbose=0)
 
     def predict_qValue(self, state):
-        state_input = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
-        with torch.no_grad():
-            q_values = self.model(state_input)
-        return q_values.cpu().numpy()
+        # Computing the q_value for the state received
+        state_input = np.expand_dims(state, axis=0)
+        q_values = self.model.predict_on_batch(state_input)
+        return q_values
 
-
+    
 class QLearner():
-    def __init__(self, env: gym.Env, max_episodes=5000, gamma=0.99, alpha=0.1, end_eps=0.01, start_eps=1.0, eps_decay=0.9999, model_name="dqn_model.pth"):
+    def __init__(self, env:gym.Env, max_episodes=3000, gamma=0.95, alpha=0.1, end_eps=0.01, start_eps=1.0,  eps_decay=0.995, model_name="dqn_model.keras"):
         self.env = env
         self.max_episodes = max_episodes        
         self.gamma = gamma
@@ -108,11 +110,11 @@ class QLearner():
         self.end_eps = end_eps
         self.eps = start_eps
         self.eps_decay = eps_decay
-        self.batch_dimension = 64
-        self.bath_capacity = 50000
+        self.batch_dimension = 32
+        self.update_modality = 0 # 0 deterministic, 1 non determinstic
         self.update_every = 4
         self.model_name = "./dqn_models/" + model_name
-
+        
     def _espilon_update(self):
         self.eps = max(self.eps_decay * self.eps, self.end_eps)
 
@@ -125,7 +127,7 @@ class QLearner():
             a = np.argmax(q_values).item()
             return a
 
-    def _prepareBatch(self, batch, q_network: DQN):
+    def _prepareBatch(self, batch, q_network: DQN, update_modality = 0):
         training_set = [] # Result of preparing the data
         
         # Preparing the batch
@@ -138,15 +140,19 @@ class QLearner():
             # Updating only in corrispondence of the action
             action = int(t[1])
             row = (t[0], q_values_s)
-            row[1][action] = t[2] + self.gamma * np.max(q_values_ns) * (1 - int(t[4]))
+            if update_modality == 0:
+                row[1][action] = t[2] + self.gamma * np.max(q_values_ns) * (1 - int(t[4]))
+            else:
+                row[1][action] = q_values_s[action] + self.alpha * (t[2] + self.gamma * np.max(q_values_ns) * (1 - int(t[4])) - q_values_s[action])
             
             training_set.append(row)
 
         return training_set
 
     def _normalize(self, state):
-        amplitudes = [2.5, 2.5, 10., 10., 6.2831855, 10., 1., 1.]
-        return state #/ amplitudes
+        amplitudes = [ 2.5, 2.5, 10., 10., 6.2831855, 10., 1., 1.]
+        result = state / amplitudes 
+        return result
 
     def DQN_Learning(self, modality=1): 
         self.eps = 1.0
@@ -155,7 +161,7 @@ class QLearner():
         q_network = DQN(8, self.env.action_space.n)
 
         # Creation of the dataset implementing the replay memory
-        memory = ReplayBuffer(self.bath_capacity)
+        memory = ReplayBuffer(10000)
 
         # Starting of the environment
         s, _ = self.env.reset()
@@ -165,7 +171,7 @@ class QLearner():
         eps_per_episode = []
 
         for n_episode in range(self.max_episodes):
-            #print(f"Episode n: {n_episode}")
+            print(f"Episode n: {n_episode}")
             episode_reward = 0
             done = False
             n_steps = 0
@@ -186,7 +192,7 @@ class QLearner():
                 if ( len(memory) > self.batch_dimension and n_steps % self.update_every == 0 ):
                     batch = memory.sample(self.batch_dimension)
                     # Preparing the batch
-                    training_set = self._prepareBatch(batch, q_network)
+                    training_set = self._prepareBatch(batch, q_network, self.update_modality)
                     q_network.train(training_set)
 
                 # Updating new state
@@ -194,51 +200,60 @@ class QLearner():
                     s = ns
             
             # Stats of the episode
+            print(f"(m={modality} episode {n_episode} {episode_reward} {self.eps})")
             eps_per_episode.append(self.eps)
             self._espilon_update()
             s, _ = self.env.reset()
             total_rewards.append(episode_reward)
-            if n_episode > 50:
-                print(f"(m={modality} episode {n_episode} {np.mean(total_rewards[-50:])} {self.eps})")
 
         if modality == 1:
             self._save_policy(q_network)
         return total_rewards, eps_per_episode
 
-    def _save_policy(self, q_network: DQN):
-        torch.save(q_network.model.state_dict(), self.model_name)
+    def _save_policy(self, q_network : DQN):
+        # Policy saving. Weigths of the neural network
+        q_network.model.save(self.model_name + ".keras")
 
-    def _load_policy(self, q_network: DQN):
-        q_network.model.load_state_dict(torch.load(self.model_name))
-        q_network.model.eval()
+    def _load_policy(self, q_network : DQN):
+        # Policy loading
+        q_network.model = tf.keras.models.load_model(self.model_name + ".keras")
 
     def run_policy(self):
+        # NN for Q-Values already trained
         q_network = DQN(8, self.env.action_space.n)
         self._load_policy(q_network)
-
+        
         total_reward = 0
         episodes_reward = []
         n_episodes = 1000
 
-        for i in range(n_episodes):
+        for i in range(0,n_episodes):
             s, _ = self.env.reset()
+            
             terminated = truncated = False
             rw = 0
+
             while not (terminated or truncated):
+                # Retrieve the action with maximum q_value
                 q_values = q_network.predict_qValue(s)[0]
                 a = np.argmax(q_values)
+
                 ns, r, terminated, truncated, _ = self.env.step(a)
                 rw += r
                 s = ns
+
             total_reward += rw
             episodes_reward.append(rw)
             print(f"({i}: rw={rw:.2f})")
-
-        print(f"Mean Reward: {total_reward / n_episodes}")
+        
+        print(f"Mean Reward: {total_reward/n_episodes}")
         print(f"Mean Episode Reward: {np.mean(episodes_reward)}")
 
 
+
 if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
     #drive.mount('/content/drive')
     #output_dir = "/content/drive/MyDrive/reward_files_dqn/"
     #os.makedirs(output_dir, exist_ok=True)
