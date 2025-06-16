35ecde54166133033e7bd5742ca53aba7f2a00ef
matteoventali <ventali.1985026@studenti.uniroma1.it>
2025-06-16 09:36:20 +0200

    Export

---
+++ 1985026-Ventali/src/lunar_lander.py
@@ -0,0 +1,272 @@
+# Authors: Matteo Ventali and Valerio Spagnoli
+# ML Project : tabular Q-learning for Lunar Lander envinronment
+
+import matplotlib.pyplot as plt
+import numpy as np
+import gymnasium as gym
+from gymnasium.spaces import Box
+from collections import defaultdict
+import random as ran
+import pickle
+
+window_size = 500
+
+def accuracy_plot(reward, types):
+    x = range(len(reward))
+    mean = np.mean(reward)
+    plt.scatter(x,reward)
+    plt.title(f'Cumulative episode reward {types} policy')
+    plt.xlabel('Episode #')
+    plt.ylabel('Episode reward')
+    plt.axhline(y=mean, color='red', linestyle='--', label=f'Mean = {mean:.2f}')
+    plt.grid(True)
+    plt.legend(loc='upper right')
+    plt.show()
+
+def function_plot_combined(reward_eps, reward_random, epsilon_value):
+    mean_mobile = np.convolve(reward_eps, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile_random = np.convolve(reward_random, np.ones(window_size)/window_size, mode="valid")
+
+    # Organizing the graph with two y axis
+    fig, ax1 = plt.subplots()
+
+    # First axis
+    ax1.plot(mean_mobile, color='red', label='reward_greedy_policy')
+    ax1.plot(mean_mobile_random, color='green', label='reward_greedy_policy')
+    ax1.set_ylabel('policies')
+    ax1.set_xlabel('episodes')
+
+    # Second axis
+    ax2 = ax1.twinx()
+    ax2.plot(epsilon_value, color='orange', label='reward_greedy_policy')
+    ax2.set_ylabel('epsilon value')
+    
+    plt.title("Learning trend")
+    plt.show()
+
+def function_plot_tuning(reward_eps1, reward_eps2, reward_eps3):
+    mean_mobile1 = np.convolve(reward_eps1, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile2 = np.convolve(reward_eps2, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile3 = np.convolve(reward_eps3, np.ones(window_size)/window_size, mode="valid")
+
+    plt.plot(mean_mobile1, color='red', label='15k_y99_9995')
+    plt.plot(mean_mobile2, color='blue', label='15k_y9_999')
+    plt.plot(mean_mobile3, color='green', label='15k_y9_99')
+    plt.ylabel('policies')
+    plt.xlabel('episodes')
+
+    plt.title("Different learning trends")
+    plt.legend(loc='upper left')
+    plt.show()
+
+def discretize(obs):
+        result = []
+        
+        # Interval array
+        x_intervals     = [-0.5, 0.5]
+        y_intervals     = [-0.1, 0.1, 1.5]
+        vx_intervals    = [-7.5, -5, -0.3, -0.1, 0.1, 0.3, 5, 7.5]
+        vy_intervals    = [-7.5, -5, -0.3, -0.1, 0.1, 0.3, 5, 7.5]
+        theta_intervals = [-1.25663706,  -0.1, 0.1, 1.25663706]
+        omega_intervals = [-7.5, -5, -0.1, 0.1, 5, 7.5]
+        
+        result.append(np.digitize(obs[0], x_intervals))
+        result.append(np.digitize(obs[1], y_intervals))
+        result.append(np.digitize(obs[2], vx_intervals))
+        result.append(np.digitize(obs[3], vy_intervals))
+        result.append(np.digitize(obs[4], theta_intervals))
+        result.append(np.digitize(obs[5], omega_intervals))
+
+        # No need to discretize boolean variables
+        result.append(int(obs[6]))
+        result.append(int(obs[7]))
+        return tuple(result)
+    
+
+class QLearner():
+    def __init__(self, env:gym.Env, max_episodes=10000, gamma=0.99, alpha=0.1, end_eps=0.01, start_eps=1.0,  eps_decay=0.9995, policy_name="policy_lunar_lander"):
+        self.env = env
+        self.max_episodes = max_episodes        
+        self.gamma = gamma
+        self.alpha = alpha
+        self.end_eps = end_eps
+        self.eps = start_eps
+        self.eps_decay = eps_decay
+        self.policy_name = "./policy/" + policy_name
+        
+    def _espilon_update(self):
+        self.eps = max(self.eps_decay * self.eps, self.end_eps)
+
+    def _next_action(self, current_state, modality):
+        n = ran.random()
+        if modality == 0  or n < self.eps: # Exploration
+            return self.env.action_space.sample()
+        else: # Exploitation
+            return np.argmax(self.q_table[current_state])
+
+    def tabular_QLearning(self, modality=1): # 0 for random, 1 for eps-greedy policy
+        # Q-table creation dynamically. It is implemented as a dictionary in which:
+        # - the keys are the states (observation collected)
+        # - the values are array of 4 components (number of actions available), representing Q(s,a)
+        # - when a state is not already in the dictionary, then it is added with a value of (0,0,0,0). This happens the first time
+        #   the agent ends up in that state
+        self.q_table = defaultdict(lambda: np.zeros(self.env.action_space.n))
+        self.eps = 1.0
+
+        # Starting of the environment
+        s, _ = self.env.reset()
+        s = discretize(s)
+
+        # Array for collecting total rewards
+        total_rewards = []
+        eps_per_episode = []
+        
+        # Training of episodes
+        for n_episode in range(self.max_episodes):
+            
+            terminated = truncated = False
+            episode_reward = 0
+            
+            while not (terminated or truncated): # Single episode
+                # Select the action to be executed
+                a = self._next_action(s, modality)
+
+                # Execution of a
+                ns, reward, terminated, truncated, _ = self.env.step(a)
+                episode_reward += reward
+                ns = discretize(ns)
+
+                # Update Q_table
+                # Q(s,a) = (1 - alpha)*Q(s,a) + alpha*[r + y max_a'{Q(s',a')}]
+                # Q(s,a) = Q(s,a) + alpha[r + y*max_a'{Q(s',a')} - Q(s,a)]
+                self.q_table[s][a] += self.alpha * (reward + self.gamma * max(self.q_table[ns]) - self.q_table[s][a])
+
+                # Updating new state
+                if not (terminated or truncated):
+                    s = ns
+            
+            # Stats of the episode
+            eps_per_episode.append(self.eps)
+            self._espilon_update()
+            s, _ = self.env.reset()
+            s = discretize(s)
+            total_rewards.append(episode_reward)
+            if n_episode > 50:
+                print(f"(m={modality} episode {n_episode} {np.mean(total_rewards[-50:])} {self.eps})")
+            
+        # Dump q_table
+        if modality == 1:
+            with open(self.policy_name, "wb") as f:
+                pickle.dump(dict(ql.q_table), f)
+
+        return np.array(total_rewards), np.array(eps_per_episode)
+    
+    def load_policy(self):
+        with open(self.policy_name, "rb") as f:
+            self.policy = defaultdict(lambda: np.zeros(self.env.action_space.n),pickle.load(f))
+        
+    def run_policy(self):
+        self.load_policy()
+        
+        episodes_reward = []
+        episodes_length = []
+        success_100 = 0 # Times reward > 100
+        success_200 = 0 # Times reward > 200
+        n_episodes = 1000
+
+        for i in range(0,n_episodes):
+            s, _ = self.env.reset()
+            s = discretize(s)
+
+            terminated = truncated = False
+            rw = 0
+            length_ep = 0
+
+            while not (terminated or truncated):
+                a = np.argmax(self.policy[s])
+                ns, r, terminated, truncated, _ = self.env.step(a)
+                length_ep += 1
+                ns = discretize(ns)
+                rw += r
+                s = ns
+
+            episodes_reward.append(rw)
+            episodes_length.append(length_ep)
+            if rw >= 100:
+                success_100 += 1
+            if rw >= 200:
+                success_200 += 1
+            
+        print(f"Mean Episode Reward: {np.mean(episodes_reward)}")
+        print(f"Success >= 100: {success_100}")
+        print(f"Success >= 200: {success_200}")
+        print(f"Mean Episode Length: {np.mean(episodes_length)}")
+        return episodes_reward
+
+    def run_random(self, n_ep=None):
+        total_reward = 0
+        episodes_reward = []
+        n_episodes = 1000
+
+        if n_ep != None:
+            n_episodes = n_ep
+        else:
+            n_episodes = self.max_episodes
+
+        for i in range(0,n_episodes):
+            s, _ = self.env.reset()
+            s = discretize(s)
+            
+            terminated = truncated = False
+            rw = 0
+
+            while not (terminated or truncated):
+                a = self._next_action(s, 0)
+                ns, r, terminated, truncated, _ = self.env.step(a)
+                ns = discretize(ns)
+                rw += r
+                s = ns
+
+            total_reward += rw
+            episodes_reward.append(rw)
+        
+        print(f"Mean Episode Reward: {np.mean(episodes_reward)}")        
+        return episodes_reward
+
+
+if __name__ == "__main__":
+    # Lunar Lander Environment
+    env = gym.make("LunarLander-v3", continuous=False, gravity=-10.0, enable_wind=False, wind_power=15.0, turbulence_power=1.5)
+    
+    # Menu
+    mode = input("Select modality (0 = training, 1 = running, 2 = tuning): ").strip()
+    policy_file = input("File policy (empty for default):").strip()
+    
+    # Learner object
+    if policy_file == "": # Apply default name
+        ql = QLearner(env)
+    else:
+        ql = QLearner(env, policy_name=policy_file)    
+
+    if mode == "0": # Training
+        rw_random = ql.run_random()
+        rw_eps, eps_values = ql.tabular_QLearning()
+        function_plot_combined(rw_eps, rw_random, eps_values)
+    elif mode == "1": # Running
+        rw_policy = ql.run_policy()
+        rw_random = ql.run_random(n_ep=1000)
+        accuracy_plot(rw_policy, 'epsilon-greedy')
+        accuracy_plot(rw_random, 'random')
+    elif mode == "2": # Tuning
+        ql = QLearner(env, policy_name="tuning_15k_y99_9995_2", max_episodes=15000, gamma=0.99, eps_decay=0.9995)
+        rw_eps1, eps_values1 = ql.tabular_QLearning()
+
+        ql2 = QLearner(env, policy_name="tuning_15k_y9_999_2", max_episodes=15000, gamma=0.9, eps_decay=0.999)
+        rw_eps2, eps_values2 = ql2.tabular_QLearning()
+
+        ql3 = QLearner(env, policy_name="tuning_15k_y9_99_2", max_episodes=15000, gamma=0.9, eps_decay=0.99)
+        rw_eps3, eps_values3 = ql3.tabular_QLearning()
+
+        function_plot_tuning(rw_eps1, rw_eps2, rw_eps3)
+    else:
+        print("Input not valid")
---
+++ 1985026-Ventali/src/lunar_lander_torch.py
@@ -0,0 +1,388 @@
+# Authors: Matteo Ventali and Valerio Spagnoli
+# ML Project : DQN for Lunar Lander environment
+
+import matplotlib.pyplot as plt
+import numpy as np
+import gymnasium as gym
+import random as ran
+from collections import deque
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+window_size = 500
+
+def plot_vector(vector, title="title", xlabel="xlabel", ylabel="ylabel"):
+    plt.plot(vector, linestyle='-')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.tight_layout()
+    plt.show()
+
+def accuracy_plot(reward, types):
+    x = range(len(reward))
+    mean = np.mean(reward)
+    plt.scatter(x,reward)
+    plt.title(f'Cumulative episode reward {types} policy')
+    plt.xlabel('Episode #')
+    plt.ylabel('Episode reward')
+    plt.axhline(y=mean, color='red', linestyle='--', label=f'Mean = {mean:.2f}')
+    plt.grid(True)
+    plt.legend(loc='upper right')
+    plt.show()
+
+def function_plot_combined(reward_eps, reward_random, epsilon_value, title):
+    mean_mobile = np.convolve(reward_eps, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile_random = np.convolve(reward_random, np.ones(window_size)/window_size, mode="valid")
+
+    # Organizing the graph with two y axis
+    fig, ax1 = plt.subplots()
+
+    # First axis
+    ax1.plot(mean_mobile, color='red', label='reward_greedy_policy')
+    ax1.plot(mean_mobile_random, color='green', label='reward_random_policy')
+    ax1.set_ylabel('policies')
+    ax1.set_xlabel('episodes')
+
+    # Second axis
+    ax2 = ax1.twinx()
+    ax2.plot(epsilon_value, color='orange', label='reward_greedy_policy')
+    ax2.set_ylabel('epsilon value', color='orange')
+    
+    plt.title(f"Learning trend with {title} update rule")
+    plt.show()
+
+def function_plot_tuning_buffer(reward_eps1, reward_eps2, reward_eps3):
+    mean_mobile1 = np.convolve(reward_eps1, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile2 = np.convolve(reward_eps2, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile3 = np.convolve(reward_eps3, np.ones(window_size)/window_size, mode="valid")
+
+    plt.plot(mean_mobile1, color='red', label='b50k')
+    plt.plot(mean_mobile2, color='blue', label='b100k')
+    plt.plot(mean_mobile3, color='green', label='b200k')
+    plt.ylabel('policies')
+    plt.xlabel('episodes')
+
+    plt.title("Different learning trends (buffer size)")
+    plt.legend(loc='upper left')
+    plt.show()
+
+def function_plot_tuning(reward_eps1, reward_eps2, reward_eps3):
+    mean_mobile1 = np.convolve(reward_eps1, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile2 = np.convolve(reward_eps2, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile3 = np.convolve(reward_eps3, np.ones(window_size)/window_size, mode="valid")
+
+    plt.plot(mean_mobile1, color='red', label='8k_y99_9995')
+    plt.plot(mean_mobile2, color='blue', label='8k_y9_999')
+    plt.plot(mean_mobile3, color='green', label='8k_y9_99')
+    plt.ylabel('policies')
+    plt.xlabel('episodes')
+
+    plt.title("Different learning trends")
+    plt.legend(loc='upper left')
+    plt.show()
+
+def function_plot_comparison(reward_det, reward_ndet):
+    mean_mobile_det = np.convolve(reward_det, np.ones(window_size)/window_size, mode="valid")
+    mean_mobile_ndet = np.convolve(reward_ndet, np.ones(window_size)/window_size, mode="valid")
+
+    plt.plot(mean_mobile_det, color='orange', label='Mean mobile deterministic update')
+    plt.plot(mean_mobile_ndet, color='blue', label='Mean mobile non deterministic update')
+    plt.ylabel('policies')
+    plt.xlabel('episodes')
+    plt.title("Comparison between update rules")
+    plt.legend()
+    plt.show()
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def add(self, state, action, reward, next_state, done):
+        data = (state, action, reward, next_state, done) 
+        self.buffer.append(data)
+
+    def sample(self, batch_size):
+        batch = ran.sample(self.buffer, batch_size)
+        return batch
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class DQN:
+    def __init__(self, state_dim, num_actions, device="cpu"):
+        self.device = device
+        self.model = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, num_actions)
+        ).to(self.device)
+        self.loss_fn = nn.MSELoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+
+    def train(self, batch):
+        states = torch.tensor(np.array([s for s, q in batch]), dtype=torch.float32).to(self.device)
+        q_targets = torch.tensor(np.array([q for s, q in batch]), dtype=torch.float32).to(self.device)
+
+        self.optimizer.zero_grad()
+        q_preds = self.model(states)
+        loss = self.loss_fn(q_preds, q_targets)
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def predict_qValue(self, state):
+        state_input = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            q_values = self.model(state_input)
+        return q_values.cpu().numpy()
+
+
+class QLearner():
+    def __init__(self, env: gym.Env, max_episodes=8000, memory_capacity=200000, gamma=0.99, alpha=0.1, end_eps=0.01, start_eps=1.0, eps_decay=0.9995, model_name="dqn_model"):
+        self.env = env
+        self.max_episodes = max_episodes        
+        self.gamma = gamma
+        self.alpha = alpha
+        self.end_eps = end_eps
+        self.eps = start_eps
+        self.eps_decay = eps_decay
+        self.batch_dimension = 64
+        self.memory_capacity = memory_capacity
+        self.update_every = 4
+        self.update_modality = 1 # 0 deterministic, 1 non determinstic
+        self.model_name = "./dqn_models/" + model_name
+
+    def _espilon_update(self):
+        self.eps = max(self.eps_decay * self.eps, self.end_eps)
+
+    def _next_action(self, modality, current_state, q_network : DQN):
+        n = ran.random()
+        if modality == 0 or n < self.eps: # Exploration
+            return self.env.action_space.sample()
+        else: # Exploitation
+            q_values = q_network.predict_qValue(current_state)[0]
+            a = np.argmax(q_values).item()
+            return a
+
+    def _prepareBatch(self, batch, q_network: DQN):
+        training_set = [] # Result of preparing the data
+        
+        # Preparing the batch
+        for t in batch: # (s, a, r, s', done)
+            # Q(s,a) and Q(s',a) forall action a
+            q_values_s = q_network.predict_qValue(t[0])[0]
+            q_values_ns = q_network.predict_qValue(t[3])[0]
+            
+            # Q(s,a) = Q(s,a) + alpha[r + y*max_a'{Q(s',a')} - Q(s,a)]
+            # Updating only in corrispondence of the action
+            action = int(t[1])
+            row = (t[0], q_values_s)
+            if self.update_modality == 0:
+                row[1][action] = t[2] + self.gamma * np.max(q_values_ns) * (1 - int(t[4])) #det
+            else:
+                row[1][action] = q_values_s[action] + self.alpha * (t[2] + self.gamma * np.max(q_values_ns) * (1 - int(t[4])) - q_values_s[action]) #nodet
+            
+            training_set.append(row)
+
+        return training_set
+
+    def _normalize(self, state):
+        amplitudes = [2.5, 2.5, 10., 10., 6.2831855, 10., 1., 1.]
+        return state / amplitudes
+
+    def DQN_Learning(self, modality=1, update_modality=1): 
+        self.eps = 1.0
+        self.update_modality = update_modality
+
+        # Creation of the NN representing the Q-table
+        q_network = DQN(8, self.env.action_space.n)
+
+        # Creation of the dataset implementing the replay memory
+        memory = ReplayBuffer(self.memory_capacity)
+
+        # Starting of the environment
+        s, _ = self.env.reset()
+
+        # Array for collecting total rewards
+        total_rewards = []
+        eps_per_episode = []
+        episodes_loss = []
+
+        for n_episode in range(self.max_episodes):
+            #print(f"Episode n: {n_episode}")
+            episode_reward = 0
+            episode_loss = []
+            done = False
+            n_steps = 0
+            while not done:
+                # Select the action to be executed
+                a = self._next_action(modality, s, q_network)
+
+                # Execution of a
+                ns, reward, terminated, truncated, _ = self.env.step(a)
+                n_steps+=1
+                episode_reward += reward
+                done = terminated or truncated
+                
+                # (s,a,r,s',done) in replay buffer
+                #memory.add(self._normalize(s), a, reward, self._normalize(ns), done)
+                memory.add(s, a, reward, ns, done)
+
+                # get a sample batch for training
+                if ( len(memory) > self.batch_dimension and n_steps % self.update_every == 0 ):
+                    batch = memory.sample(self.batch_dimension)
+                    # Preparing the batch
+                    training_set = self._prepareBatch(batch, q_network)
+                    loss = q_network.train(training_set)
+                    episode_loss.append(loss)
+
+                # Updating new state
+                if not done:
+                    s = ns
+            
+            # Stats of the episode
+            episodes_loss.append(np.mean(episode_loss))
+            eps_per_episode.append(self.eps)
+            self._espilon_update()
+            s, _ = self.env.reset()
+            total_rewards.append(episode_reward)
+            if n_episode > 50:
+                print(f"(m={self.update_modality} episode {n_episode} {np.mean(total_rewards[-50:])} {self.eps} {episodes_loss[n_episode]})")
+
+        if update_modality == 1:
+            self._save_policy(q_network, title = "_ndet.pth")
+        elif update_modality == 0:
+            self._save_policy(q_network, title = "_det.pth")
+        return total_rewards, eps_per_episode, episodes_loss
+
+    def _save_policy(self, q_network: DQN, title= None):
+        torch.save(q_network.model.state_dict(), self.model_name + title)
+
+    def _load_policy(self, q_network: DQN):
+        q_network.model.load_state_dict(torch.load(self.model_name))
+        q_network.model.eval()
+
+    def run_policy(self):
+        q_network = DQN(8, self.env.action_space.n)
+        self._load_policy(q_network)
+
+        episodes_reward = []
+        episodes_length = []
+        success_100 = 0 # Times reward > 100
+        success_200 = 0 # Times reward > 200
+        n_episodes = 1000
+
+        for i in range(n_episodes):
+            s, _ = self.env.reset()
+            terminated = truncated = False
+            length_ep = 0
+            rw = 0
+
+            while not (terminated or truncated):
+                q_values = q_network.predict_qValue(s)[0]
+                a = np.argmax(q_values)
+                length_ep += 1
+                ns, r, terminated, truncated, _ = self.env.step(a)
+                rw += r
+                s = ns
+            
+            episodes_reward.append(rw)
+            episodes_length.append(length_ep)
+            if rw >= 100:
+                success_100 += 1
+            if rw >= 200:
+                success_200 += 1
+
+        print(f"Mean Episode Reward: {np.mean(episodes_reward)}")
+        print(f"Success >= 100: {success_100}")
+        print(f"Success >= 200: {success_200}")
+        print(f"Mean Episode Length: {np.mean(episodes_length)}")
+        return episodes_reward
+        return episodes_reward
+
+    def run_random(self, n_ep=None):
+        total_reward = 0
+        episodes_reward = []
+        
+        if n_ep != None:
+            n_episodes = n_ep
+        else:
+            n_episodes = self.max_episodes
+
+        for i in range(0,n_episodes):
+            s, _ = self.env.reset()
+            
+            terminated = truncated = False
+            rw = 0
+
+            while not (terminated or truncated):
+                a = self.env.action_space.sample()
+                ns, r, terminated, truncated, _ = self.env.step(a)
+                rw += r
+                s = ns
+
+            total_reward += rw
+            episodes_reward.append(rw)
+        
+        print(f"Mean Episode Reward: {np.mean(episodes_reward)}")        
+        return episodes_reward
+
+
+if __name__ == "__main__":
+    # Lunar Lander Environment
+    env = gym.make("LunarLander-v3", continuous=False, gravity=-10.0, enable_wind=False, wind_power=15.0, turbulence_power=1.5)
+    
+    # Menu
+    mode = input("Select modality (0 = training, 1 = running, 2 = tuning): ").strip()
+    model_file = input("File model (empty for default):").strip()
+
+    # Learner object
+    if model_file == "": # Apply default name
+        ql = QLearner(env)
+    else:
+        ql = QLearner(env, model_name=model_file)
+
+    if mode == "0": # Training
+        rw_random = ql.run_random() 
+        rw_eps_det, eps_values_det, det_loss = ql.DQN_Learning(update_modality=0)
+        rw_eps_ndet, eps_values_ndet, ndet_loss = ql.DQN_Learning(update_modality=1)
+        function_plot_combined(rw_eps_det, rw_random, eps_values_det, title="det")
+        function_plot_combined(rw_eps_ndet, rw_random, eps_values_ndet, title="non det")
+        function_plot_comparison(rw_eps_det, rw_eps_ndet)
+        plot_vector(ndet_loss, title="Loss trend with non-det update", xlabel="episodes", ylabel="mean loss")
+        plot_vector(det_loss, title="Loss trend with det update", xlabel="episodes", ylabel="mean loss")
+    elif mode == "1": # Running
+        rw_policy = ql.run_policy()
+        rw_random = ql.run_random(n_ep = 1000)
+        accuracy_plot(rw_policy, 'epsilon-greedy')
+        accuracy_plot(rw_random, 'random')
+    elif mode == "2": # Tuning
+        ql = QLearner(env, model_name="buffer_tuning_8k_y99_9995_b50k", gamma=0.99, eps_decay=0.9995, memory_capacity=50000)
+        buffer_rw_eps1, _, _ = ql.DQN_Learning()
+        
+        ql = QLearner(env, model_name="buffer_tuning_8k_y99_9995_b100k", gamma=0.99, eps_decay=0.9995, memory_capacity=100000)
+        buffer_rw_eps2, _, _ = ql.DQN_Learning()
+
+        ql = QLearner(env, model_name="buffer_tuning_8k_y99_9995_b200k", gamma=0.99, eps_decay=0.9995, memory_capacity=200000)
+        buffer_rw_eps3, _, _ = ql.DQN_Learning()
+
+        ql = QLearner(env, model_name="tuning_8k_y99_9995", gamma=0.99, eps_decay=0.9995)
+        rw_eps1, _, _ = ql.DQN_Learning()
+        
+        ql = QLearner(env, model_name="tuning_8k_y9_999", gamma=0.9, eps_decay=0.999)
+        rw_eps2, _, _ = ql.DQN_Learning()
+
+        ql = QLearner(env, model_name="tuning_8k_y9_99", gamma=0.9, eps_decay=0.99)
+        rw_eps3, _, _ = ql.DQN_Learning()
+
+        function_plot_tuning_buffer(buffer_rw_eps1, buffer_rw_eps2, buffer_rw_eps3)
+        function_plot_tuning(rw_eps1, rw_eps2, rw_eps3)
+    else:
+        print("Input not valid")
